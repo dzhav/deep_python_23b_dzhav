7797831f39c5c06684b1b17d6eea7d37bbd10967
dzhav <fataliev.ds@phystech.edu>
2023-10-12 16:18:57 +0300

    3_task_1.0

---
+++ 02/CustomList.py
@@ -0,0 +1,75 @@
+class CustomList(list):
+    def __str__(self):
+        return f"{list(self)} ({sum(self)})"
+        
+    def __add__(self, other_list):
+        if isinstance(other_list, list):
+            other_list = CustomList(other_list)
+        ans = CustomList()
+        for i in range(0, max(len(self), len(other_list))):
+            if i < len(self) and i < len(other_list):
+                ans.append(self[i] + other_list[i])
+            elif i < len(self):
+                ans.append(self[i])
+            else:
+                ans.append(other_list[i])
+        return ans
+
+    def __radd__(self, other_list):
+        return self + other_list
+
+    def __sub__(self, other_list):
+        if isinstance(other_list, list):
+            other_list = CustomList(other_list)
+        ans = CustomList()
+        for i in range(0, max(len(self), len(other_list))):
+            if i < len(self) and i < len(other_list):
+                ans.append(self[i] - other_list[i])
+            elif i < len(self):
+                ans.append(self[i])
+            else:
+                ans.append(-other_list[i])
+        return ans
+
+    def __rsub__(self, other_list):
+        if isinstance(other_list, list):
+            other_list = CustomList(other_list)
+        ans = CustomList()
+        for i in range(0, max(len(self), len(other_list))):
+            if i < len(self) and i < len(other_list):
+                ans.append(other_list[i] - self[i])
+            elif i < len(other_list):
+                ans.append(other_list[i])
+            else:
+                ans.append(-self[i])
+        return ans
+
+    def __eq__(self, other_list):
+        if isinstance(other_list, list):
+            other_list = CustomList(other_list)
+        return sum(self) == sum(other_list)
+
+    def __ne__(self, other_list):
+        if isinstance(other_list, list):
+            other_list = CustomList(other_list)
+        return sum(self) != sum(other_list)
+
+    def __gt__(self, other_list):
+        if isinstance(other_list, list):
+            other_list = CustomList(other_list)
+        return sum(self) > sum(other_list)
+
+    def __ge__(self, other_list):
+        if isinstance(other_list, list):
+            other_list = CustomList(other_list)
+        return sum(self) >= sum(other_list)
+
+    def __lt__(self, other_list):
+        if isinstance(other_list, list):
+            other_list = CustomList(other_list)
+        return sum(self) < sum(other_list)
+
+    def __le__(self, other_list):
+        if isinstance(other_list, list):
+            other_list = CustomList(other_list)
+        return sum(self) <= sum(other_list)
---
+++ 02/testCustomList.py
@@ -0,0 +1,47 @@
+import unittest
+from CustomList import CustomList
+
+class TestCustomList(unittest.TestCase):
+    def setUp(self):
+        print("SETUP")
+
+    def tearDown(self):
+        print("TEAR_DOWN")
+
+    def test_add(self):
+        lst1 = CustomList([1, 2, 3])
+        lst2 = CustomList([4, 5, 6])
+        lst3 = CustomList([7, 8, 9])
+        self.assertEqual(lst1 + [4, 5], CustomList([5, 7, 3]))
+        self.assertEqual(lst1 + lst2, CustomList([5, 7, 9]))
+        self.assertEqual(lst1 + [4, 5, 6], CustomList([5, 7, 9]))
+        self.assertEqual([4, 5, 6] + lst1, CustomList([5, 7, 9]))
+        self.assertEqual(lst1 + lst3, CustomList([8, 10, 12]))
+
+    def test_subtract(self):
+        lst1 = CustomList([1, 2, 3])
+        lst2 = CustomList([4, 5, 6])
+        self.assertEqual(lst1 - lst2, CustomList([-3, -3, -3]))
+        self.assertEqual(lst1 - [4, 5, 6], CustomList([-3, -3, -3]))
+        self.assertEqual([4, 5, 6] - lst1, CustomList([3, 3, 3]))
+        self.assertEqual([4, 5, 6] - CustomList([1, 2]), CustomList([3, 3, 6]))
+
+    def test_comparison(self):
+        lst1 = CustomList([1, 2, 3])
+        lst2 = CustomList([14, 15, 10])
+        lst3 = CustomList([1, 2, 3])
+        self.assertTrue(lst2 > lst1)
+        self.assertTrue(lst2 >= lst1)
+        self.assertTrue(lst1 < lst2)
+        self.assertTrue(lst1 <= lst2)
+        self.assertTrue(lst1 == lst3)
+        self.assertTrue(lst1 != lst2)
+
+    def test_str(self):
+        lst = CustomList([1, 2, 3])
+        lst2 = CustomList([1, 2, 4])
+        self.assertEqual(str(lst2), "[1, 2, 4] (7)")
+        self.assertEqual(str(lst), "[1, 2, 3] (6)")
+
+if __name__ == "__main__":
+    unittest.main()
